dc3a0886b8ec250a0ddc087130df6104d9b8e395
csidyel <46521788+csidyel@users.noreply.github.com>
2023-09-19 12:55:44 +0200

    Update test.sh

--- .github/workflows/ci/test.sh
+++ .github/workflows/ci/test.sh
@@ -18,14 +18,14 @@ bundle exec rake assets:precompile
 echo "Running basic rspec tests..."
 bundle exec rake zammad:db:init
 #bundle exec rspec --exclude-pattern "spec/system/**/*_spec.rb" -t ~searchindex -t ~integration -t ~required_envs
-#gem install semaphore_test_boosters
-#TEST_BOOSTERS_RSPEC_TEST_EXCLUDE_PATTERN='spec/system/**/*_spec.rb' TB_RSPEC_OPTIONS="-t ~searchindex -t ~integration -t ~required_envs" rspec_booster --job "${JOB_INDEX}/${JOB_COUNT}"
+gem install semaphore_test_boosters
+TEST_BOOSTERS_RSPEC_TEST_EXCLUDE_PATTERN='spec/system/**/*_spec.rb' TB_RSPEC_OPTIONS="-t ~searchindex -t ~integration -t ~required_envs" rspec_booster --job "${JOB_INDEX}/${JOB_COUNT}"
 
 gem install knapsack_pro
 bundle exec rake "knapsack_pro:rspec[-t ~searchindex -t ~integration -t ~required_envs]"
 
 echo "Running basic minitest tests..."
 bundle exec rake zammad:db:reset
-bundle exec rake knapsack_pro:minitest
+#bundle exec rake knapsack_pro:minitest
 #bundle exec rake knapsack_pro:test_unit
-#bundle exec rake test:units
+bundle exec rake test:units
