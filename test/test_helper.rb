b2c24ca8a64464ea3f4e6b36fbb6650c4e62f7cf
csidyel <46521788+csidyel@users.noreply.github.com>
2023-09-19 12:38:53 +0200

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,6 @@
 
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../config/environment', __dir__)
-require 'knapsack_pro'
-
-# Custom Knapsack Pro config here
-
-knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
-knapsack_pro_adapter.set_test_helper_path(__FILE__)
 require 'rails/test_help'
 require 'minitest/profile'
 
