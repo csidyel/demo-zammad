dba701a8aeffbfc42bee14302bde5a6d4d6917e6
csidyel <46521788+csidyel@users.noreply.github.com>
2023-10-25 10:40:13 +0200

    Update ticket_biz_break_spec.rb

--- spec/lib/escalation/ticket_biz_break_spec.rb
+++ spec/lib/escalation/ticket_biz_break_spec.rb
@@ -144,9 +144,6 @@ RSpec.describe Escalation::TicketBizBreak, time_zone: 'Europe/Berlin' do
       it { expect(result.values[4].keys).to eq %w[00:00] }
       it { expect(result.values[4]['00:00']).to eq '24:00' }
       it { expect(result.values[5].keys).to eq %w[00:00] }
-      #it { expect(result.values[5]['00:00']).to eq '12:00' }
-      xcontext 'skip failing case' do
-        it { expect(result.values[5]['00:00']).to eq '12:00' }
       end
     end
   end
